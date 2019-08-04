505f7df175a514cc61c9a7b736ecd405729451d0
nan_glb <nangood_glb@qq.com>
2019-08-04 19:10:52 +0800

    Initial commit

---
+++ papapig_user/src/main/java/com/pppig/user/UserApplication.java
@@ -0,0 +1,18 @@
+package com.pppig.user;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@MapperScan("com.pppig.user.dao")
+public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
+
+
+	
+}
